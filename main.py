63d959090e48116efa69b3861a50a7f94fa8b13b
Kaler654 <63152258+Kaler654@users.noreply.github.com>
2021-04-19 00:22:18 +0300

    Added moderators

--- main.py
+++ main.py
@@ -572,9 +572,8 @@ def logout():
 
 def main():
     db_session.global_init("db/data.db")
-    # port = int(os.environ.get("PORT", 5000))
-    # app.run(host='0.0.0.0', port=port)
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
