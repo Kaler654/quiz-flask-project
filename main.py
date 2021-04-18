2fab9ffd2d5161dafa1175eb3c29ea08c96ea3ef
Kaler654 <63152258+Kaler654@users.noreply.github.com>
2021-04-18 23:29:40 +0300

    Heroku

--- main.py
+++ main.py
@@ -569,7 +569,8 @@ def logout():
 
 def main():
     db_session.global_init("db/data.db")
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
